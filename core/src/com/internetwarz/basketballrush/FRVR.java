decd7f03b5d6ca1880d39fe9f690a2432ac0afe3
lazermann111 <lazermann111@gmail.com>
2017-10-20 14:58:20 +0300

    -ball is falling through  the window :D

--- core/src/com/internetwarz/basketballrush/FRVR.java
+++ core/src/com/internetwarz/basketballrush/FRVR.java
@@ -9,8 +9,15 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.internetwarz.basketballrush.utils.GameUtils;
 import com.internetwarz.basketballrush.utils.Score;
 import com.internetwarz.basketballrush.utils.SimpleDirectionGestureDetector;
@@ -24,6 +31,9 @@ public class FRVR implements Screen,InputProcessor{
 
     GlyphLayout layoutScore;
 
+    World world;
+    Body ballBody;
+
     //setting values on touch
     private static int gameSpeed;
     private static int touchCounter;
@@ -34,20 +44,18 @@ public class FRVR implements Screen,InputProcessor{
 
     //setting variables to store two different color balls and the net
     private Texture targetNet;     //Contains net image resource
-    private Texture playerBallTexture;       //Contains ball texture
-    private Rectangle palyerBallRectangle;   // ball physic body
+    private Texture playerBallTexture;       // Contains ball texture
+    private Sprite ballSprite;
 
 
-    public FRVR(final BasketBallRush gam){
+    public FRVR(final BasketBallRush gam)
+    {
         this.game = gam;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, appWidth, appHeight);
-
         batch = new SpriteBatch();
         batch.setProjectionMatrix(camera.combined);
-
         layoutScore = new GlyphLayout();
-
 
         InputMultiplexer plex = new InputMultiplexer();
         plex.addProcessor(this);
@@ -80,7 +88,6 @@ public class FRVR implements Screen,InputProcessor{
         }));
         Gdx.input.setInputProcessor(plex);
 
-
         gameSpeed = 600;
         touchCounter =0;
         gameutils = new GameUtils();
@@ -88,46 +95,81 @@ public class FRVR implements Screen,InputProcessor{
 
         //loading the images in the variables
         targetNet = game.assets.getTexture("net_frvr");
+
+
+        // Center the sprite in the top/middle of the screen
+
+
         playerBallTexture = game.assets.getTexture("ball_frvr");
+        ballSprite = new Sprite(playerBallTexture);
+        ballSprite.setPosition(Gdx.graphics.getWidth() / 2 - ballSprite.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2);
 
 
-        //placing the player dot in the middle of the screen
-        palyerBallRectangle = new Rectangle(appWidth/2 - targetNet.getWidth()/2,20, targetNet.getWidth(), targetNet.getHeight());
 
 
+        // Create a physics world, the heart of the simulation.  The Vector
+        //passed in is gravity
+        world = new World(new Vector2(0, -98f), true);
+
+        // Now create a BodyDefinition.  This defines the physics objects type
+        //and position in the simulation
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        // We are going to use 1 to 1 dimensions.  Meaning 1 in physics engine
+        //is 1 pixel
+        // Set our body to the same position as our sprite
+        bodyDef.position.set(ballSprite.getX(), ballSprite.getY());
+
+        // Create a body in the world using our definition
+        ballBody = world.createBody(bodyDef);
+
+        // Now define the dimensions of the physics shape
+        PolygonShape shape = new PolygonShape();
+        // We are a box, so this makes sense, no?
+        // Basically set the physics polygon to a box with the same dimensions
+        //as our sprite
+        shape.setAsBox(ballSprite.getWidth()/2, ballSprite.getHeight()/2);
+
+        // FixtureDef is a confusing expression for physical properties
+        // Basically this is where you, in addition to defining the shape of the
+       // body
+        // you also define it's properties like density, restitution and others
+        //we will see shortly
+        // If you are wondering, density and area are used to calculate over all
+        //mass
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1f;
+
+        Fixture fixture = ballBody.createFixture(fixtureDef);
+
+        // Shape is the only disposable of the lot, so get rid of it
+        shape.dispose();
     }
 
-    /*private void populateDots(){
-        Rectangle dots = new Rectangle();
-        dots.x = gameutils.randomLocation((int)appWidth);
-        dots.y = appHeight;
-        dots.width = playerBallTexture.getWidth();
-        dots.height = playerBallTexture.getHeight();
-
-        Random rand = new Random();
-        int  n = rand.nextInt(2) + 1;
-        GameBallValues g;
-        if(n==1){
-            g = new GameBallValues(dots,gameDotImage1,"image1");
-        }
-        else{
-            g = new GameBallValues(dots, playerBallTexture,"image");
-        }
-        gameDot.add(g);
-
-        lastDotTime = TimeUtils.nanoTime();
-    }*/
 
     @Override
     public void render(float delta) {
+
+
+        // Advance the world, by the amount of time that has elapsed since the
+        //last frame
+        // Generally in a real game, dont do this in the render loop, as you are
+        //tying the physics
+        // update rate to the frame rate, and vice versa
+        world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+
+        // Now update the spritee position accordingly to it's now updated
+       // Physics body
+        ballSprite.setPosition(ballBody.getPosition().x, ballBody.getPosition().y);
+
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-
         batch.begin();
 
         //Drawing the net image
-        batch.draw(targetNet, palyerBallRectangle.x, palyerBallRectangle.y, palyerBallRectangle.width, palyerBallRectangle.height);
+        batch.draw(ballSprite, ballSprite.getX(), ballSprite.getY()/*, ballSprite.getWidth(), ballSprite.getHeight()*/);
 
         layoutScore.setText(game.font,""+score.getStringScore());
         if(touchCounter >= 1) {
@@ -144,8 +186,12 @@ public class FRVR implements Screen,InputProcessor{
 
     @Override
     public void dispose() {
+        // Hey, I actually did some clean up in a code sample!
         batch.dispose();
         game.dispose();
+        targetNet.dispose();
+        playerBallTexture.dispose();
+        world.dispose();
     }
 
     @Override
